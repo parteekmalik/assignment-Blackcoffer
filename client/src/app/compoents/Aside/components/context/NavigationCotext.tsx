8ffe90e50b7e5ae3f3626cd4dfc86094709e326f
parteekmalik <parteekmalik5@gmail.com>
2024-05-25 22:54:03 +0530

    removed trash

--- client/src/app/compoents/Aside/components/context/NavigationCotext.tsx
+++ client/src/app/compoents/Aside/components/context/NavigationCotext.tsx
@@ -18,40 +18,14 @@ const NavigationCotextCoponent: React.FC<{
   isdebug: boolean;
 }> = ({ children, isdebug }) => {
   const { pathname } = useLocation();
-  const pathList = pathname.split('/');
   useEffect(() => setcurrNavigation(pathname), [pathname]);
 
   const [currNavigation, setcurrNavigation] = useState('');
-  const updateNavigation = () => {};
-  const initializeNavigation = (list: NestedList[]) => {
-    const map = new Map<string, boolean>();
 
-    const flatList = [];
-    const flatListFn = ({
-      list,
-      link,
-      depth,
-    }: {
-      depth: number;
-      list?: NestedList[];
-      link: string;
-    }) => {
-      list?.forEach((i) => {
-        if (link === pathList.slice(0, depth).join('/')) map.set(link, true);
-        flatListFn({
-          list: i.list,
-          link: link + '/' + i.name.toLowerCase().replace(' ', ''),
-          depth: depth + 1,
-        });
-      });
-    };
-    flatListFn({ list, link: '/', depth: 0 });
-  };
   function modifyPath(link: string) {
     setcurrNavigation((prev) => {
       console.log('setcurrNavigation', prev, link, pathname);
       if (link === pathname) return pathname;
-      // if ( return '';
       if (
         prev === link ||
         machPartialString({ currNavigation: prev, link: link })
@@ -83,14 +57,7 @@ export function machPartialString({
   link: string;
 }) {
   const len = link.length;
-  // console.log(
-  //   'machPartialString',
-  //   currNavigation.slice(0, len),
-  //   link.slice(0, len),
-  //   currNavigation.slice(0, len) === link.slice(0, len),
-  //   currNavigation,
-  //   link
-  // );
+
   if (currNavigation.slice(0, len) === link.slice(0, len)) return true;
   return false;
 }
