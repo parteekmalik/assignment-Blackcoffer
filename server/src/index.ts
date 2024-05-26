a232c32fa10700d9dd3500e1302f7eddd9c92b2c
parteekmalik <parteekmalik5@gmail.com>
2024-05-26 20:23:26 +0530

    lgtm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
-import express from 'express';
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const prisma = new PrismaClient();
+
 const app = express();
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/all", async (req, res) => {
+    res.json(await prisma.data.findMany());
 });
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+    return console.log(`Express is listening at http://localhost:${port}`);
 });
