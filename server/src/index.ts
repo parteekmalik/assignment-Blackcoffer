4be9f8c6de2b92d95711073ba2ae1f71015a1fba
parteekmalik <parteekmalik5@gmail.com>
2024-05-28 03:36:13 +0530

    lgtm

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,13 +17,19 @@ const Filter: Record<string, string[] | { gte: number; lte: number }> = {};
 getFilter(Filter, prisma);
 
 app.get("/api", async (req, res) => {
-    const skip = parseInt(req.query.skip as string, 10) || 0; // Default to 0 if not provided
-    const take = parseInt(req.query.take as string, 10) || 10; // Default to 10 if not provided
-    const FilterData = req.query.FilterData === "true" ? true : false; // Default to 10 if not provided
+    const { skip: S, take: T, FilterData: F, ...rest } = req.query;
+    const skip = parseInt(S as string, 10) || 0; // Default to 0 if not provided
+    const take = parseInt(T as string, 10) || 10; // Default to 10 if not provided
+    const FilterData = F === "true" ? true : false; // Default to 10 if not provided
 
-    const data = await prisma.data.findMany({ skip, take });
-    console.log("req recieved", data.length, Filter);
-    res.json(FilterData ? { data, FilterData: Filter } : { data });
+    try {
+        const data = await prisma.data.findMany({ skip, take, ...rest });
+        console.log("req received", data.length, Filter);
+        res.status(200).json(FilterData ? { data, FilterData: Filter } : { data });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 app.get("/api/getNavData", async (req, res) => {
     res.json(NavData);
